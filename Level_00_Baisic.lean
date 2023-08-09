fc0d1c6ffd72eeff47ac818223b6b53437d4a0d3
KathyS01 <ks2021@ic.ac.uk>
2023-08-09 20:02:43 +0000

    0, 1, +, x

--- Level_00_Baisic.lean
+++ Level_00_Baisic.lean
@@ -1,9 +1,20 @@
-import data.real.basic
-
 structure complex where
-re : ℝ
-im : ℝ
+re : Nat
+im : Nat
 
 notation "ℂ" => complex
 
 #check complex.mk 10 20
+
+def zero : ℂ := ⟨0, 0⟩
+
+instance : HasZero ℂ := ⟨zero⟩
+
+def one : ℂ := ⟨1, 0⟩
+
+instance : has_one ℂ := ⟨one⟩
+
+def add (z w : ℂ) : ℂ := ⟨z.re + w.re, z.im + w.im⟩
+
+def mul (z w : ℂ) : ℂ := ⟨z.re * w.re - z.im * w.im, z.re * w.im + z.im * w.re⟩
+
